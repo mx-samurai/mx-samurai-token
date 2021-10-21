decbef3fb08a09aa392f362a41201b07f8560e0a
thecryptocowboy <cryptocowboy1337@github.com>
2021-10-21 19:56:06 -0400

    Update deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,14 @@ const hre = require("hardhat");
 const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
 async function main() {
-  const MXSToken = await hre.ethers.getContractFactory("MXSToken");
-  const mxsToken = await MXSToken.deploy(UNISWAP_ROUTER);
+    const arr1 = [
+        "0x16E7451D072eA28f2952eefCd7cC4A30B1F6A557", 
+        "0x9acF9DBdEebDF65057A10b7CaD2a90397df6D9D9", 
+        "0xAC57E5c7990aa2A7a3B91A0F9d4350127e07DEC5", 
+        "0xa5F1ff86a73B2e15d1aE2652e81fA0b7Fada314C"
+    ];
+    const MXSToken = await hre.ethers.getContractFactory("MXSToken");
+    const mxsToken = await MXSToken.deploy(UNISWAP_ROUTER);
 
   await mxsToken.deployed();
   console.log("Token deployed to", mxsToken.address);
@@ -15,6 +21,39 @@ async function main() {
 
   console.log("Vesting Router deployed to", vestingRouter.address);
 
+
+  await mxsToken.excludeFromReward("0xEB1B1fB761A336DF83283333280Ab39F4289aDD5");
+  await mxsToken.excludeFromReward("0xD5f5E93594A90F603D2FE8f6BABdC8F8DF590dcF");
+  console.log("Token configured: Exclusions from Rewards set.");
+
+  await mxsToken.excludeFromFee("0xEB1B1fB761A336DF83283333280Ab39F4289aDD5");
+  await mxsToken.excludeFromFee("0xD5f5E93594A90F603D2FE8f6BABdC8F8DF590dcF");
+  console.log("Token configured: Exclusions from Fees set.");
+
+  await mxsToken.excludeFromBlockLimit("0xEB1B1fB761A336DF83283333280Ab39F4289aDD5");
+  await mxsToken.excludeFromBlockLimit("0xD5f5E93594A90F603D2FE8f6BABdC8F8DF590dcF");
+  console.log("Token configured: Exclusions from Block Limit set.");
+
+  await mxsToken.allowPreTrading("0xEB1B1fB761A336DF83283333280Ab39F4289aDD5", true);
+  await mxsToken.allowPreTrading("0xD5f5E93594A90F603D2FE8f6BABdC8F8DF590dcF", true);
+  console.log("Token configured: Pretrade whitelist list set.");
+
+//   await mxsToken.setTradingStartTime("1635621804000");
+//   console.log("Token configured: Start time set.");
+
+  await mxsToken.setMaxTxAmount("100000000000000000000000000000");
+  console.log("Token configured: Max transaction amount set.");
+
+  const tokenTransfer = await mxsToken.transfer(vestingRouter.address, 12500000009);
+  await tokenTransfer.wait();
+
+  console.log("Transferred 12500000000 tokens to the Vesting Router.");
+
+//   await vestingRouter.createVesting("0x16E7451D072eA28f2952eefCd7cC4A30B1F6A557", "1000000000000000000000000000", "7776000", "0", "false");
+  for (var i = 0; i < arr1.length; i++) {
+    await vestingRouter.createVesting(arr1[i], "1000000000000000000000000000", "7776000", "0", "false");
+    console.log("Vesting configured for " + arr1[i]);
+    }
 
 }
 
